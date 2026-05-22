6dd5f9eef22c98014c19533555c389b4bc2134b5
SantiagoBC31 <sbustosc@ucundinamarca.edu.co>
2026-05-22 15:48:04 -0500

    Refactor model route and remove decision tree route

--- app.py
+++ app.py
@@ -3,27 +3,49 @@ import pandas as pd
 
 app = Flask(__name__)
 
+# =========================
+# HOME
+# =========================
 @app.route('/')
 def home():
     return render_template('mainMenu.html')
 
+
+# =========================
+# OBJECTIVES
+# =========================
 @app.route('/pests/objectives')
 def pests_objectives():
     return render_template('objectives.html')
 
+
+# =========================
+# DATA ENGINEERING
+# =========================
 @app.route('/pests/dataEngineering')
 def pests_data():
     return render_template('dataEngineering.html')
 
+
+# =========================
+# MODEL ENGINEERING
+# =========================
 @app.route('/pests/modelEngineering')
 def pests_model():
+
+    # CSV GENERAL
     predictions = pd.read_csv("predictions.csv")
-    return render_template('modelEngineering.html',
-                           predictions = predictions.to_dict(orient='records'))
 
-@app.route('/models/development/decision-tree')
-def decision_tree_development():
-    return render_template('decisionTreeDevelopment.html')
+   
+    return render_template(
+        'modelEngineering.html',
+        predictions=predictions.to_dict(orient='records'),
+      
+    )
 
+
+# =========================
+# APP
+# =========================
 if __name__ == '__main__':
     app.run(debug=True)
